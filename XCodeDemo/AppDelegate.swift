89eb79aae180386a1e4c8c48fff7f00fa4c18c8e
Mark Andrew <mchugh@ozemail.com.au>
2019-01-12 22:54:42 +1100

    Initial Commit

---
+++ XCodeDemo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  XCodeDemo
+//
+//  Created by Mark McHugh on 12/1/19.
+//  Copyright © 2019 Mark Andrew. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
