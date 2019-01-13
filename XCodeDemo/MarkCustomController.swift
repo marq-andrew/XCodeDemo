9fc0aab800bb95c9f0c125512f744956a619afc4
Mark Andrew <mchugh@ozemail.com.au>
2019-01-13 15:30:15 +1100

    ui3

---
+++ XCodeDemo/MarkCustomController.swift
@@ -0,0 +1,13 @@
+//
+//  MarkCustomController.swift
+//  XCodeDemo
+//
+//  Created by Mark McHugh on 13/1/19.
+//  Copyright © 2019 Mark Andrew. All rights reserved.
+//
+
+import Foundation
+
+class customController : NSObject {
+    
+}
